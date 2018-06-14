23cfce1dfcf2e1dd4c243b57cf23f3cf43f485f9
Unknown <klapil.filip@seznam.cz>
2018-06-15 00:51:47 +0200

    to do version for get adc data

--- FW_ADC/main.c
+++ FW_ADC/main.c
@@ -9,9 +9,12 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/pgmspace.h>
+#include <stdio.h>
 
 #include "uart.h"
 #include "i2c.h"
+
 
 #define ADC_ADDRESS 0b10010000 
 
@@ -31,27 +34,31 @@ int main(void)
     
     setup();
     
+    char buffer[10];
     unsigned char ret;
+    uint8_t buffer_i2c[2];
     
     while (1) 
     {
         PORTB ^= (1<<5);
         //uart_puts("pokus\n");
-        ret = i2c_start(ADC_ADDRESS + I2C_WRITE);
-        i2c_stop();
-        uart_puts("pokus\n");
+        ret = i2c_start(ADC_ADDRESS + I2C_READ);
+        //uart_puts("pokus\n");
         if ( ret )
         {
+            i2c_stop();
             uart_puts("error\n");
         }
         else
         {
-            uart_puts("started OK\n");
+            msb_voltage = i2c_readAck();
+            lsb_voltage = i2c_readNak();
+            voltage = ((lsb_voltage>>2) + (msb_voltage>>6));
+            i2c_stop();
+            sprintf_P(buffer, PSTR("%d\n"),voltage);
+            uart_puts(buffer);
         }
         _delay_ms(1000);
-        
-        
-        uart_puts("pokus\n");
     }
 }
 
